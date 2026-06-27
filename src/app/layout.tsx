29c0e3526938485d70a55e02ff1397fd623fb85e
mostafa-512 <engmostafaabdalraheem@gmail.com>
2026-06-27 14:38:20 +0300

    Fixing something relaited to MetaTages and Web Link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,6 @@ export const metadata: Metadata = {
   verification: {
     google: 'J6jwNRu1fkQBYQ-482U_YRKx-zZqb2mc4xvKFxdU4hk',
   },
-  // 2. Twitter Cards (Added - Uses your OpenGraph details for X/Twitter sharing)
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Mostafa Abdalraheem | Front-end Developer',
-    description: 'Portfolio of Mostafa Abdalraheem, a Front-end Developer specializing in React.js and Next.js.',
-    images: ['/IMG.JPG'], 
-  },
   
   // 3. Crawler Instructions (Added - Ensures Google indexes the site correctly)
   robots: {
@@ -30,20 +23,27 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://mostafa-abdalraheem.netlify.app/'),
   openGraph: {
     title: 'Mostafa Abdalraheem | Front-end Developer',
     description: 'Portfolio of Mostafa Abdalraheem, a Front-end Developer specializing in React.js and Next.js.',
-    url: 'https://mostafa-abdalraheem.netlify.app/', // Replace with the Real URL of Your Website
+    url: process.env.NEXT_PUBLIC_SITE_URL || 'https://mostafa-abdalraheem.netlify.app/',
     siteName: 'Mostafa Portfolio',
     images: [
       {
-        url: '/IMG.JPG', 
+        url: '/IMG.JPG',
         width: 1200,
         height: 630,
       },
     ],
     locale: 'en_US',
     type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Mostafa Abdalraheem | Front-end Developer',
+    description: 'Portfolio of Mostafa Abdalraheem, a Front-end Developer specializing in React.js and Next.js.',
+    images: ['/IMG.JPG'],
   },
 };
 
